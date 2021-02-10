2ca5a3f2a089d9b313852e245f6ace0c5cf45694
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-02-10 22:02:39 +0100

    align pad names

--- rtl/udma_uart_wrap.sv
+++ rtl/udma_uart_wrap.sv
@@ -17,6 +17,7 @@
  *
  */
  module udma_uart_wrap
+    import udma_pkg::udma_evt_t;
 (
     input  logic         sys_clk_i,
     input  logic         periph_clk_i,
@@ -29,15 +30,15 @@
 	output logic         cfg_ready_o,
     output logic  [31:0] cfg_data_o,
 
-    output logic  [3:0]  events_o,
+    output udma_evt_t    events_o,
 
     // UDMA CHANNEL CONNECTION
     UDMA_LIN_CH.rx_out   rx_ch[0:0],
     UDMA_LIN_CH.tx_in    tx_ch[0:0],
 
     // PAD SIGNALS CONNECTION
-	BIPAD_IF.PERIPH_SIDE pad_uart_rx,
-	BIPAD_IF.PERIPH_SIDE pad_uart_tx
+	BIPAD_IF.PERIPH_SIDE PAD_UART_RX,
+	BIPAD_IF.PERIPH_SIDE PAD_UART_TX
 
 
 );
@@ -49,8 +50,8 @@ udma_uart_top #(.L2_AWIDTH_NOAL(L2_AWIDTH_NOAL), .TRANS_SIZE(TRANS_SIZE)) i_udma
     .sys_clk_i          (sys_clk_i       ),
     .periph_clk_i       (periph_clk_i    ),
     .rstn_i             (rstn_i          ),
-    .uart_rx_i          (pad_uart_rx.IN  ),
-    .uart_tx_o          (pad_uart_tx.OUT ),
+    .uart_rx_i          (PAD_UART_RX.IN  ),
+    .uart_tx_o          (PAD_UART_TX.OUT ),
     .rx_char_event_o    (events_o[0]     ),
     .err_event_o        (events_o[1]     ),
     
@@ -101,9 +102,9 @@ assign events_o[2] = 1'b0;
 assign events_o[3] = 1'b0;
 
 // freezing pad direction
-assign pad_uart_rx.OE = 1'b0;
-assign pad_uart_rx.OUT = 1'b0;
-assign pad_uart_tx.OE = 1'b1;
+assign PAD_UART_RX.OE = 1'b0;
+assign PAD_UART_RX.OUT = 1'b0;
+assign PAD_UART_TX.OE = 1'b1;
 
 // assigning unused signals
 assign rx_ch[0].stream = '0;
